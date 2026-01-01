0d3fb20b4a6735ac6c1929730b76641d48661e6e
Taha Mansuri <tahamansuri@Tahas-MacBook-Pro.local>
2026-01-01 07:55:01 +0500

    Naming changes

--- backend/routes/attendanceroute.js
+++ backend/routes/attendanceroute.js
@@ -1,10 +1,16 @@
- const express = require('express');
- const router = express.Router();
- const authenticateToken = require('../middleware/authentication');
- const {checkIn,checkOut,ping,todayStatus,history} = require('../controller/attendance');
-router.post("/check-in" , authenticateToken, checkIn);
+const express = require("express");
+const router = express.Router();
+const authenticateToken = require("../middleware/authentication");
+const {
+  checkIn,
+  checkOut,
+  ping,
+  todayStatus,
+  history,
+} = require("../controller/attendance");
+router.post("/check-in", authenticateToken, checkIn);
 router.post("/check-out", authenticateToken, checkOut);
 router.post("/ping", authenticateToken, ping);
 router.get("/today-status", authenticateToken, todayStatus);
 router.get("/history", authenticateToken, history);
- module.exports = router;
+module.exports = router;
--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -1,13 +1,20 @@
- const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const pool = require("../config/db");
-  const {signup,verifyOTP,login,refreshToken,forgotPassword,resetPassword} = require('../controller/auth');
-  
-  router.post('/signup', signup);
-router.post('/verify-otp', verifyOTP);
-router.post('/login', login);
-router.post('/refresh-token', refreshToken);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
-  module.exports = router;
+const {
+  signup,
+  verifyOTP,
+  login,
+  refreshToken,
+  forgotPassword,
+  resetPassword,
+} = require("../controller/auth");
+
+router.post("/signup", signup);
+router.post("/verify-otp", verifyOTP);
+router.post("/login", login);
+router.post("/refresh-token", refreshToken);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+module.exports = router;

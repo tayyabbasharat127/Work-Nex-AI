4bc753749a1c1beccf545b9b65200ee77c508c44
Taha Junaid <tahamansuri04@gmail.com>
2026-01-17 16:51:45 +0500

    Save progress before switching branches

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ const pool = new Pool({
   user: 'postgres',          // or hr_admin (if you created that user)
   host: 'localhost',
   database: 'WORKNEX-AI',
-  password: 'underrated123', // your PostgreSQL password
+  password: '123', // your PostgreSQL password
   port: 5432,
 });
 
-// Test connection
+// Test connectioncd
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error(' PostgreSQL connection error:', err);
